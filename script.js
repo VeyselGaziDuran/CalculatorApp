fbcc4870c0ef6e72c1e116c0b0ee534c28c71786
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-06-11 19:53:05 +0300

    js button values ​​received, AC button activated, dot setting activated

--- script.js
+++ script.js
@@ -1,63 +1,52 @@
-const display = document.querySelector('.calculator-input');
-const keys = document.querySelector('.calculator-keys');
+const display = document.querySelector(".calculator-input");
+const keys = document.querySelector(".calculator-keys");
 
 let displayValue = '0';
 
 updateDisplay();
 
-function updateDisplay() {
-  display.value = displayValue;
-}
+function updateDisplay(){
+    display.value = displayValue;
+};
 
-
-keys.addEventListener('click', (e) => {
+keys.addEventListener('click', function(e){
     const element = e.target;
-    const value = element.value;
 
-    if (!element.matches('button')) {
-        return;
-    }
+    if(!element.matches('button')) return;
 
-    if (element.classList.contains('operator')) {
+
+    if(element.classList.value.includes("operator")){
         console.log('operator', element.value);
         return;
     }
 
-    if (element.classList.contains('decimal')) {
-        inputDecimal(element.value);
-        return;
-    }
 
-    if (element.classList.contains('all-clear')) {
-        console.log('clear', element.value);
-        clearAll();
+    if(element.classList.value.includes('decimal')){
+        inputDecimal(element.value);
         updateDisplay();
         return;
     }
 
+    if(element.classList.value.includes('clear')){
+        clear();
+        updateDisplay()
+        return;
+    }
+
     inputNumber(element.value);
+    updateDisplay();
 });
 
-function inputNumber(num) {
-    if (displayValue === '0') {
-        displayValue = num;
-    } else {
-        displayValue += num;
+function inputNumber(num){
+    displayValue = displayValue === '0' ? num: displayValue + num
+}
+
+function inputDecimal(dot){
+    if(!displayValue.includes('.')){
+    displayValue += ".";
     }
-
-    updateDisplay();
 }
 
-function inputDecimal(dot) {
-    if (!displayValue.includes(dot)) {
-        displayValue += dot;
-    }
-
-    updateDisplay();
+function clear(clear){
+    displayValue = '0'
 }
-
-function clearAll() {
-    displayValue = '0';
-    updateDisplay();
-}
-
